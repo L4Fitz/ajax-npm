b245f493f0874f461b676ca33d91d78abea0ce73
L4Fitz <93418853+L4Fitz@users.noreply.github.com>
2023-04-15 23:38:23 +0700

    Delete src directory

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import './styles/style.css';
-import 'regenerator-runtime';
-import './script/component/app-bar.js';
-import main from './script/view/main.js';
-
-document.addEventListener('DOMContentLoaded', main);
